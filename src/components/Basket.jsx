60e38e6860ec9db09ac6adfd5d118e869f426e41
Sabine <sabi4826@stud.kea.dk>
2022-11-09 13:20:44 +0100

    5cartstotal works - at least the total

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,10 +1,19 @@
 
 function Basket(props) {
+    function getTotal() {
+        let total = 0;
+        props.cart.forEach(item=> {
+            total+=item.amount * item.price;
+        });
+        return total;
+    }
     <section className="Basket">
         <ul>
             {props.cart.map(item=><li>{item.productdisplayname} x {item.amount}, {item.amount * item.price};</li>)}
             
         </ul>
+        <h3>Total: {getTotal()}</h3>
+        <button>Buy now</button>
     </section>
 }
 
