8c6a37aa9a64b2a4c5fb2f9dd713cc824895d3a8
Sabine <sabi4826@stud.kea.dk>
2022-11-11 14:13:46 +0100

    Images finally loading (remember back ticks!)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,38 +1,16 @@
-import React from "react";
-
 function Product(props) {
 
     function add() {
         props.addToCart(props.data);
     }
 return (
-    <article className="Product">
+  <article className="Product">
     <h2>{props.data.productdisplayname}</h2>
     <p>{props.data.price},-</p>
-    <img src={'https://kea-alt-del.dk/t7/images/webp/640/${props.data.id}.webp'} alt={props.data.productdisplayname} />
+    <img src={`https://kea-alt-del.dk/t7/images/webp/640/${props.data.id}.webp`} alt={props.data.productdisplayname}/>
     <button onClick={add}>Add to basket</button>
-</article>)
+  </article>
+)
 }
 
 export default Product;
-
-/* 
-{
-  "products": [
-    {
-      "id": 1163,
-      "gender": "Men",
-      "category": "Apparel",
-      "subcategory": "Topwear",
-      "articletype": "Tshirts",
-      "season": "Summer",
-      "productionyear": 2011,
-      "usagetype": "Sports",
-      "productdisplayname": "Sahara Team India Fanwear Round Neck Jersey",
-      "price": 895,
-      "discount": null,
-      "brandname": "Nike",
-      "soldout": 0
-    },
-}
-*/
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,8 @@
 import Product from "./Product";
 
 function ProductList(props) {
-    return (<main className="Products">
+  return (
+  <main className="Products">
    {props.products.map((product) => (
     <Product key={product.id} {...product} data={product} addToCart={props.addToCart}/>
    ))}
